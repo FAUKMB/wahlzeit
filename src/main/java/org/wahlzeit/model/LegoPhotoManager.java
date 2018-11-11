49c150096d9eedb98f1baccc4cdb3379e80a23ed
KMB <kili.bender@t-online.de>
2018-11-11 20:09:36 +0100

    updated LegoPhotoManager

--- src/main/java/org/wahlzeit/model/LegoPhotoManager.java
+++ src/main/java/org/wahlzeit/model/LegoPhotoManager.java
@@ -13,24 +13,7 @@ package org.wahlzeit.model;
 public class LegoPhotoManager extends PhotoManager {
 	
 	public LegoPhotoManager() {
-		photoTagCollector = LegoPhotoFactory.getInstance().createPhotoTagCollector();
+		super();
 	}
 	
-	@Override
-	public Photo getPhotoFromId(PhotoId id) {
-		if (id == null) {
-			return null;
-		}
-
-		Photo result = doGetPhotoFromId(id);
-
-		if (result == null) {
-			result = LegoPhotoFactory.getInstance().loadPhoto(id);
-			if (result != null) {
-				doAddPhoto(result);
-			}
-		}
-
-		return result;
-	}
 }
