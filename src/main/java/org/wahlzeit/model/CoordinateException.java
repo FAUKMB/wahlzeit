2c5f9cf2a4ac1c71cddd5b3dd6321b8f72062344
KMB <kili.bender@t-online.de>
2018-12-09 19:37:02 +0100

    added CoordinateException

---
+++ src/main/java/org/wahlzeit/model/CoordinateException.java
@@ -0,0 +1,23 @@
+/*
+ * CoordinateException
+ * 
+ * version 1.0
+ * 
+ * Date: 8.12.18
+ * 
+ * License: AGPLv3
+ */
+
+package org.wahlzeit.model;
+
+public class CoordinateException extends Exception{
+	
+	public CoordinateException() {
+		
+	}
+	
+	public CoordinateException(String message) {
+		super(message);
+	}
+
+}
